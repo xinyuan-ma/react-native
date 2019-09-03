6bbac60ce2c174cc600176c0b9d86ebc11f71690
BWH\Chris.Ma <chris.ma@bestwehotel.com>
2019-09-03 19:23:20 +0800

    ’修改首页样式

--- abcd/App.js
+++ abcd/App.js
@@ -58,7 +58,7 @@ const styles = StyleSheet.create ({
         fontSize: 18,
         marginLeft: 15,
         marginBottom: 15,
-        marginTop: 40
+        marginTop: 30
     },
     detail: {
         fontSize: 12,
